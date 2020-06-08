5ff1e805b47863f90232cf215a8388b54d61eb91
kazshinohara <kzs@google.com>
2020-06-08 14:26:29 +0900

    added redis integration

--- hwpy/app.py
+++ hwpy/app.py
@@ -1,13 +1,17 @@
 import os
+import redis
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
+redis_client = redis.Redis(host = '10.118.164.123',port = 6379, decode_responses = True)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'World')
+    redis_client.set('key1', 'value1')
+    target = redis_client.get('key1')
+    # target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
     # host_header = request.headers.get("Host")
     # if host_header:
